a68a986b333ece8111ecc87fb7601f230c6636bd
Sora-Shiro <sora95shiro@gmail.com>
2019-03-16 12:55:29 +0800

    sketch App

--- src/BackgroundTask.js
+++ src/BackgroundTask.js
@@ -1,4 +1,6 @@
-// TODO: 需要深入了解 async、await、Promise
+// TODO: 需要深入了解 async、await、Promise 和 import、export
+// 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements/import
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements/export
 export function saveEditDetail(detailData) {
     return new Promise(resolve => {
         setTimeout(() => {
