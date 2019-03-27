fe4e604e34450d72b1a70fb074b27b18d5204367
Sora-Shiro <sora95shiro@gmail.com>
2019-03-27 16:40:36 +0800

    generateSHA512 optimize

--- src/Tools.js
+++ src/Tools.js
@@ -2,22 +2,16 @@ import CryptoJS from 'crypto-js';
 
 export function generateSHA512(plainText, length) {
     const key = CryptoJS.SHA512(Math.random()).toString();
-    let ciphertext = CryptoJS.HmacSHA512(plainText, key).toString();
     let result = "";
-    if (length <= 128) {
-        result += ciphertext.substring(0, length);
-    } else {
-        result += ciphertext;
-        let remainLength = length - 128;
-        while (remainLength > 0) {
-            ciphertext = CryptoJS.HmacSHA512(plainText, key).toString();
-            if (remainLength > 128) {
-                result += ciphertext;
-            } else {
-                result += ciphertext.substring(0, remainLength);
-            }
-            remainLength -= 128;
+    let remainLength = length;
+    while (remainLength > 0) {
+        const ciphertext = CryptoJS.HmacSHA512(plainText, key).toString();
+        if (remainLength > 128) {
+            result += ciphertext;
+        } else {
+            result += ciphertext.substring(0, remainLength);
         }
+        remainLength -= 128;
     }
     return result;
 }
