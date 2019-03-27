2c046b903700e1e767103ad6b152000d4afb47c5
sora95shiro <sora95shiro@gmail.com>
2019-03-27 13:17:32 +0800

    sketch footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,14 +3,16 @@ require('bootstrap');
 
 function Footer() {
   return (
-    <div class="border-top d-flex">
-      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-        Button with data-target
-      </button>
-      <div class="collapse" id="collapseExample">
-        <div class="card card-body">
+    <div class="">
+      <div class="collapse border-top" id="collapseExample">
+        <div class="card-body">
           Just test
         </div>
+      </div>
+      <div class="py-2 border-top">
+        <button class="btn btn-link text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          Button with data-target
+        </button>
       </div>
     </div>
   )
