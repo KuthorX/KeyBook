0e2b696bb18fdd96485eb915e66cae3d4c127455
sora95shiro <sora95shiro@gmail.com>
2019-03-28 13:06:27 +0800

    sketch rangeslider

--- src/Footer.js
+++ src/Footer.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from 'react';
 // https://github.com/whoisandy/react-rangeslider
 import Slider from 'react-rangeslider'
-import 'react-rangeslider/lib/index.css'
+import './rangeslider/css/rangeslider.css'
 
-function GeneratePasswordView() {
-  const [v, setV] = useState(10);
+function GeneratePasswordView(props) {
+  const pwLen = props.pwLen;
 
   function onVChange(value) {
-    setV(value);
+    props.setPwLen(value);
   }
 
   return (
     <div class="collapse border-top" id="collapseExample">
       <div class="card-body">
         <div>
-          <Slider 
-          value={v}
-          orientation="horizontal"
-          onChange={onVChange}
+          <Slider class=""
+            value={pwLen}
+            min={1}
+            max={1024}
+            orientation="horizontal"
+            onChange={onVChange}
           />
         </div>
       </div>
@@ -27,10 +29,14 @@ function GeneratePasswordView() {
 
 function Footer() {
 
-  
+  const [pwLen, setPwLen] = useState(16);
+
   return (
     <div class="">
-      <GeneratePasswordView />
+      <GeneratePasswordView
+        pwLen={pwLen}
+        setPwLen={setPwLen}
+      />
       <div class="py-2 border-top">
         <button class="btn btn-link text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
           Password Generate
