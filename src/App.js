101ddb02b3eb40275bb1a048ed8d8282dc34e370
sora95shiro <sora95shiro@gmail.com>
2019-03-29 13:12:03 +0800

    sketch OpenFilePage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
-import Aside from './Aside';
-import Detail from './Detail';
-import EditDetail from './EditDetail';
+import Content from './Content';
 import { SearchByText } from './Search';
 import WarningToast from './toast/WarningToast';
 import DeleteModal from './DeleteModal';
 import Spinners from './Spinners';
 import $ from 'jquery';
-import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import OpenFilePage from './OpenFilePage';
 require('bootstrap');
 
 // DONE: 已添加功能
@@ -28,211 +26,6 @@ require('bootstrap');
 // - 关联文件功能
 // - 中文语言支持
 // - 目录功能
-
-function Content(props) {
-  const allData = props.allData;
-  const showAccountId = props.showAccountId;
-  const showOption = props.showOption;
-
-  useEffect(() => {
-    if (allData.length === 0) {
-      setCurrentDetailIndex(-1);
-      setDetailData(null);
-      setSavePreEditData(null);
-    } else {
-      let finalIndex = -1;
-      if (showAccountId !== null) {
-        for (let i = 0; i < allData.length; i++) {
-          if (showAccountId === allData[i].id) {
-            finalIndex = i;
-            break;
-          }
-        }
-      }
-      setCurrentDetailIndex(finalIndex);
-      if (finalIndex >= 0) {
-        setSavePreEditData(JSON.parse(JSON.stringify(allData[finalIndex])));
-        setDetailData(JSON.parse(JSON.stringify(allData[finalIndex])));
-      }
-    }
-  }, [allData]);
-
-  const [savePreEditData, setSavePreEditData] = useState(null);
-  const [detailData, setDetailData] = useState(null);
-  const [currentDetailIndex, setCurrentDetailIndex] = useState(-1);
-  useEffect(() => {
-    if (currentDetailIndex >= 0) {
-      setSavePreEditData(JSON.parse(JSON.stringify(allData[currentDetailIndex])));
-      setDetailData(JSON.parse(JSON.stringify(allData[currentDetailIndex])));
-    } else {
-      setDetailData(null);
-      setSavePreEditData(null);
-    }
-  }, [currentDetailIndex]);
-
-  function discardChanges() {
-    props.setEdit(false);
-    setDetailData(JSON.parse(JSON.stringify(savePreEditData)));
-    allData[currentDetailIndex] = JSON.parse(JSON.stringify(savePreEditData));
-    props.setShowAccountId(savePreEditData.id);
-    props.setAllData(JSON.parse(JSON.stringify(allData)));
-  }
-  function saveChanges(detailData) {
-    props.setEdit(false);
-    setSavePreEditData(JSON.parse(JSON.stringify(detailData)));
-    allData[currentDetailIndex] = JSON.parse(JSON.stringify(detailData));
-    props.setShowAccountId(detailData.id);
-    props.setAllData(JSON.parse(JSON.stringify(allData)));
-  }
-
-  function addAccount() {
-    if (isEdit) {
-      props.showWaringToast("Your current account is editing, please save or cancel editing status! ");
-      return;
-    }
-    props.addAccount();
-    props.setEdit(true);
-  }
-
-  function onAccountAddClick() {
-    addAccount();
-  }
-  function onPwItemClick(name, index) {
-    if (index === currentDetailIndex) {
-      props.setShowOption("detail");
-      return;
-    }
-    if (isEdit) {
-      props.showWaringToast("Your current account is editing, please save or cancel editing status! ");
-      return;
-    }
-    setCurrentDetailIndex(index);
-    props.setShowOption("detail");
-  }
-  function onBackClick() {
-    props.setShowOption("account");
-  }
-
-  function onItemAddClick() {
-    const newDetailList = [...detailData.detailList, {
-      "id": Math.random(),
-      "label": "",
-      "value": "",
-    }];
-    const newDetailData = {
-      "name": detailData.name,
-      "id": detailData.id,
-      "tags": detailData.tags,
-      "detailList": newDetailList
-    }
-    setDetailData(newDetailData);
-  }
-  function onItemDeleteClick(index) {
-    const newDetailList = detailData.detailList.filter((item, j) => {
-      return j !== index
-    });
-    const newDetailData = {
-      "name": detailData.name,
-      "tags": detailData.tags,
-      "detailList": newDetailList
-    }
-    setDetailData(newDetailData);
-  }
-  function onEDOkClick(detailData) {
-    saveChanges(detailData);
-  }
-  function onEDCancelClick() {
-    discardChanges();
-  }
-  function onDetailEditClick() {
-    props.setEdit(true);
-  }
-  function onDetailDeleteClick() {
-    props.showDeleteModal(currentDetailIndex);
-  }
-  function onItemInputLabelChange(value, index) {
-    detailData.detailList[index].label = value;
-    setDetailData(JSON.parse(JSON.stringify(detailData)));
-  }
-  function onItemInputValueChange(value, index) {
-    detailData.detailList[index].value = value;
-    setDetailData(JSON.parse(JSON.stringify(detailData)));
-  }
-  function onItemInputTagsChange(value) {
-    detailData.tags = value;
-    setDetailData(JSON.parse(JSON.stringify(detailData)));
-  }
-  function onItemNameChange(value) {
-    detailData.name = value;
-    setDetailData(JSON.parse(JSON.stringify(detailData)));
-  }
-
-  const isEdit = props.isEdit
-  let detail;
-  if (isEdit) {
-    detail =
-      <EditDetail
-        detailData={detailData}
-        onOkClick={onEDOkClick}
-        onCancelClick={onEDCancelClick}
-        onItemAddClick={onItemAddClick}
-        onItemDeleteClick={onItemDeleteClick}
-        onItemNameChange={onItemNameChange}
-        onItemInputLabelChange={onItemInputLabelChange}
-        onItemInputValueChange={onItemInputValueChange}
-        onItemInputTagsChange={onItemInputTagsChange}
-      />;
-  } else {
-    detail =
-      <Detail
-        detailData={detailData}
-        onEditClick={onDetailEditClick}
-        onDeleteClick={onDetailDeleteClick}
-        onBackClick={onBackClick}
-      />;
-  }
-
-  let finalShow;
-  if (showOption === "account") {
-    finalShow = <div class="row">
-      <div class="col-sm-3 border-right">
-        <Aside
-          {...props}
-          data={allData}
-          onPwItemClick={onPwItemClick}
-          activeIndex={currentDetailIndex}
-          onAccountAddClick={onAccountAddClick}
-        />
-      </div>
-      <div class="col-sm d-none d-sm-block">
-        {detail}
-      </div>
-    </div>
-  } else if (showOption === "detail") {
-    finalShow = <div class="row">
-      <div class="col-sm-3 d-none d-sm-block border-right">
-        <Aside
-          {...props}
-          data={allData}
-          onPwItemClick={onPwItemClick}
-          activeIndex={currentDetailIndex}
-          onAccountAddClick={onAccountAddClick}
-        />
-      </div>
-      <div class="col-sm d-sm-block">
-        {detail}
-      </div>
-    </div>
-  }
-
-  return (
-    <Router>
-      <div class="container-fluid pw-content border-bottom">
-        {finalShow}
-      </div>
-    </Router>
-  )
-}
 
 function App() {
   const warningToastId = "warningToastId";
@@ -420,6 +213,9 @@ function App() {
       <footer>
         <Footer />
       </footer>
+      <OpenFilePage 
+        
+      />
       <DeleteModal
         modalId={deleteModalId}
         onNoClick={onDeleteModalNoClick}
@@ -434,39 +230,6 @@ function App() {
       />
     </div>
   )
-}
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { error: null, errorInfo: null };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    // Catch errors in any components below and re-render with error message
-    this.setState({
-      error: error,
-      errorInfo: errorInfo
-    })
-    // You can also log error messages to an error reporting service here
-  }
-
-  render() {
-    if (this.state.errorInfo) {
-      // Error path
-      return (
-        <div>
-          <h2>Something went wrong.</h2>
-          <details style={{ whiteSpace: 'pre-wrap' }}>
-            {this.state.error && this.state.error.toString()}
-            <br />
-            {this.state.errorInfo.componentStack}
-          </details>
-        </div>
-      );
-    }
-    // Normally, just render children
-    return this.props.children;
-  }
 }
 
 export default App;
---
+++ src/OpenFilePage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function OpenFilePage() {
+    return (
+        <div class="position-absolute vw-100 vh-100 bg-white">
+
+        </div>
+    );
+}
+
+export default OpenFilePage;
