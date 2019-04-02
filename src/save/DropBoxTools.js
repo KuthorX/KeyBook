3090477f899b7fd0c9c304056faba3e53a724b1a
kuthorx <kuthorx@gmail.com>
2019-04-02 23:48:51 +0800

    dropbox

--- src/save/DropBoxTools.js
+++ src/save/DropBoxTools.js
@@ -5,7 +5,6 @@ export function openAuthPage() {
         fetch: fetch,
         clientId: '8w80g7hb3h4ahb5'
     });
-    console.log(client);
     var authUrl = client.getAuthenticationUrl("http://localhost:3000/auth");
     window.open(authUrl);
 }
@@ -28,17 +27,42 @@ export function saveFileToDropBox(token, content, okCb, errorCb) {
         .then(okCb, errorCb);
 }
 
-export function openFileFromDropBox(token) {
+export function openFileFromDropBox(token, okCb, errorCb) {
     var fetch = require('isomorphic-fetch');
     var Dropbox = require('dropbox').Dropbox;
     new Dropbox({
         fetch: fetch,
         accessToken: token,
     })
-        .fileRequestsList()
+        .filesListFolder({ "path": "" })
         .then(file_requests => {
-            console.log(file_requests);
+            let entries = file_requests["entries"];
+            let path = "";
+            for (let i = 0; i < entries.length; i++) {
+                if (entries[i]["name"] === "data.json") {
+                    path = entries[i]["path_display"];
+                }
+            }
+            if (path === "") {
+                return errorCb("cannot find any");
+            }
+            new Dropbox({
+                fetch: fetch,
+                accessToken: token,
+            })
+                .filesDownload({ "path": path })
+                .then(file_requests => {
+                    let blob = file_requests["fileBlob"];
+                    var reader = new FileReader();
+                    reader.addEventListener("loadend", function (event) {
+                        let re = event.target.result;
+                        okCb(re);
+                    });
+                    reader.readAsText(blob, 'utf-8');
+                }, error => {
+                    errorCb(error);
+                });
         }, error => {
-            console.log(error);
+            errorCb(error);
         });
 }
